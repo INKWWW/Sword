da0ded1ffd9e3303df54ddfca5bfbfeb79740496
INKWWW <whmink@foxmail.com>
2018-09-18 19:24:10 +0800

    update

--- jz_offer/replace_space.py
+++ jz_offer/replace_space.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""替换空格"""
 
 class Solution:
     def replaceSpace(self, s):
--- jz_offer/reverse_ArrayList.py
+++ jz_offer/reverse_ArrayList.py
@@ -4,6 +4,7 @@
 #         self.val = x
 #         self.next = None
 
+"""从尾到头打印链表"""
 class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
     def printListFromTailToHead(self, listNode):  # listNode为该链表的头结点
