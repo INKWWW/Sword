dc0c203201061e03763ce767e54c6a3784c468ce
INKWWW <whmink@foxmail.com>
2018-09-19 09:59:55 +0800

    update

--- jz_offer/two_stack_array.py
+++ jz_offer/two_stack_array.py
@@ -11,9 +11,13 @@ class Solution:
         self.stack1 = []
         self.stack2 = []
     def push(self, node):
-        
+        self.stack2.append(node)
     def pop(self):
         # return xx 
-
+        if self.stack2 == []:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
+        return self.stack2.pop()
 
 
